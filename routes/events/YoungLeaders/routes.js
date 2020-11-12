4405d727bbada12b287e0c4fa0b7dbc9d9fb66b3
Pulkit Aggarwal <54775856+Pulkit0110@users.noreply.github.com>
2020-11-12 18:14:42 +0530

    Add files via upload

---
+++ routes/events/YoungLeaders/routes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const {body} = require('express-validator/check');
+
+const User = require('../../../models/events/YoungLeaders/user');
+const authController = require('../../../controllers/events/YoungLeaders/auth');
+const mainController = require('../../../controllers/events/YoungLeaders/mainController');
+const isauth = require('../../../middleware/is-auth');
+
+const router = express.Router();
+
+// router.get('/signup', authController.getSignup);
+
+// router.get('/login', authController.getLogin);
+
+router.post('/signup',[
+    body('email')
+    .isEmail()
+    .withMessage('Please enter a valid email.')
+    .custom((value,{req}) => {
+        return User.findOne({email: value})
+        .then(user => {
+            if(user) {
+                return Promise.reject('E-mail address already exists.');
+            }
+        });
+    })
+    .normalizeEmail()
+], authController.signup);
+
+router.post('/login', authController.login);
+
+router.get('/question/:questionNum',isauth,mainController.getQuestion);
+
+router.post('/checkAnswer',isauth,mainController.checkAnswer);
+
+router.post('/submit',isauth,mainController.submit);
+
+router.get('/leaderboard',isauth,mainController.getLeaderboard);
+
+module.exports = router;
