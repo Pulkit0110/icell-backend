f3e16c2ff305fccfeb3af20e9309d1c0cf0037cd
victoryvivek <vivekaggarwalthegreat1998@gmail.com>
2020-11-14 10:38:50 +0530

    setup

---
+++ constants.js
@@ -0,0 +1,2 @@
+export const USER_TABLE = "userTable";
+export const QUESTION_TABLE = "questionTable";
--- controllers/events/activeEvents.js
+++ controllers/events/activeEvents.js
@@ -1,3 +1,31 @@
 const EventModel = require("../../models/event");
 
 exports.getActiveEvents = (req, res) => {};
+
+exports.addEvents = (req, res) => {
+  const eventName = req.body.eventName;
+  const activeStatus = req.body.activeStatus;
+
+  const event = EventModel.create({
+    name: eventName,
+    isActive: activeStatus,
+    userTableName: "userTable",
+    questionTableName: "questionTable"
+  });
+  console.log(event);
+
+  // if(activeStatus)
+};
+
+const generateName = eventName => {
+  const timestamp = new Date().getTime();
+  return eventName + timestamp;
+};
+
+exports.setActiveStatus = (req, res) => {
+  let eventName = req.body.eventName;
+  const tableName = generateName(eventName);
+  const userTableName = "user_" + tableName;
+  const questionTableName = "questions_" + tableName;
+};
+exports.setInactiveStatus = (res, req) => {};
