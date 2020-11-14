d17ee6fe358c0424e1f1692afc96111d5381766b
Pulkit Aggarwal <54775856+Pulkit0110@users.noreply.github.com>
2020-11-14 11:28:46 +0530

    Add files via upload

---
+++ routes/admin/youngLeaders.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const router = express.Router();
+
+const youngleadersController = require('../../controllers/admin/youngLeaders');
+const isauth = require('../../middleware/is-auth');
+
+const multer = require('multer');
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+var upload = multer({storage: fileStorage, fileFilter: fileFilter});
+
+// router.get('/add-question',youngleadersController.getAddQuestion);
+
+router.post('/add-question',isauth,upload.single('image'),youngleadersController.postAddQuestion);
+
+router.post('/edit-question',isauth, upload.single('image'),youngleadersController.postEditQuestion);
+
+router.post('/delete-question',isauth, youngleadersController.postDeleteQuestion);
+
+module.exports = router;
