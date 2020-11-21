657ae8399eae8d8463ae6cc46683f9230331cc4a
victoryvivek <vivekaggarwalthegreat1998@gmail.com>
2020-11-21 21:48:56 +0530

    added false success

--- controllers/aboutUs.js
+++ controllers/aboutUs.js
@@ -3,7 +3,6 @@ const Sponsors = require("../models/sponsors");
 exports.getAboutUs = (req, res, next) => {
   Sponsors.find()
     .then(sponsors => {
-      // console.log(sponsor[0].imageUrl);
       res.status(200).json({
         success: true,
         sponsors: sponsors
--- controllers/events/eventInfo.js
+++ controllers/events/eventInfo.js
@@ -1,43 +1,51 @@
 const EventInfoModel = require("../../models/eventInfo");
 
 exports.getEventDescription = async (req, res) => {
-    const eventName = req.params.eventName;
+  const eventName = req.params.eventName;
 
-    const eventInfo = await EventInfoModel.find({
-        name: eventName,
-    });
+  const eventInfo = await EventInfoModel.find({
+    name: eventName
+  });
 
-    return res.json({
-        success: true,
-        eventInfo: eventInfo,
+  if (eventInfo == null) {
+    res.json({
+      success: false,
+      message: "Event not found",
+      eventInfo: {}
     });
+  }
+
+  return res.json({
+    success: true,
+    eventInfo: eventInfo
+  });
 };
 
 exports.addEventDescription = async (req, res) => {
-    const eventName = req.body.eventName;
-    const numberOfUsers = req.body.numberOfUsers;
-    const prizes = req.body.prizes;
-    const info = req.body.info;
-    const startTime = req.body.startTime;
-    const endTime = req.body.endTime;
-    const imageUrl = req.body.imageUrl;
-    const rules = req.body.rules;
-    const faqs = req.body.faqs;
+  const eventName = req.body.eventName;
+  const numberOfUsers = req.body.numberOfUsers;
+  const prizes = req.body.prizes;
+  const info = req.body.info;
+  const startTime = req.body.startTime;
+  const endTime = req.body.endTime;
+  const imageUrl = req.body.imageUrl;
+  const rules = req.body.rules;
+  const faqs = req.body.faqs;
 
-    await EventInfoModel.create({
-        name: eventName,
-        numberOfUsers: numberOfUsers,
-        prizes: prizes,
-        info: info,
-        startTime: startTime,
-        endTime: endTime,
-        imageUrl: imageUrl,
-        rules: rules,
-        faqs: faqs,
-    });
+  await EventInfoModel.create({
+    name: eventName,
+    numberOfUsers: numberOfUsers,
+    prizes: prizes,
+    info: info,
+    startTime: startTime,
+    endTime: endTime,
+    imageUrl: imageUrl,
+    rules: rules,
+    faqs: faqs
+  });
 
-    return res.json({
-        success: true,
-        message: "event info added",
-    });
+  return res.json({
+    success: true,
+    message: "event info added"
+  });
 };
